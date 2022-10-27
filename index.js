708b61af52da145394a1269ea375687b28c68cf2
sunsettdevv <johnaboy96@gmail.com>
2022-10-26 21:25:53 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,34 @@
+const { Client, Collection, GatewayIntentBits } = require("discord.js");
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.DirectMessages, GatewayIntentBits.MessageContent] });
+const { token, clientId, guildId } = require('./config.json');
+
+const fs = require('node:fs');
+const path = require('node:path');
+//client.commands = new Collection();
+
+//const commandsPath = path.join(__dirname, 'commands');
+//const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+const eventsPath = path.join(__dirname, 'events');
+const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
+
+client.login(token);
+
+/*
+for (const file of commandFiles) {
+	const filePath = path.join(commandsPath, file);
+	const command = require(filePath);
+	//client.commands.set(command.name, command);
+	command.name.forEach(name => client.commands.set(name, command));
+}
+*/
+for (const file of eventFiles) {
+	const filePath = path.join(eventsPath, file);
+	const event = require(filePath);
+	if (event.once) {
+		client.once(event.name, (...args) => event.execute(...args));
+	} else {
+		client.on(event.name, (...args) => event.execute(...args));
+	}
+}
+
+module.exports.client = client;
